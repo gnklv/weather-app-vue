0ab4a8c4748b8b8dbc7f7ef92b8c0d1fdf308835
gnklv <wgg357@gmail.com>
2020-12-21 04:37:06 +0500

    feat: created svg sprite

---
+++ src/components/ui/svg/UiSvgIcon.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg">
+    <use :href="`#icon-${iconId}`" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'UiSvgIcon',
+  props: {
+    iconId: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
