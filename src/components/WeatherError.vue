26596c2235b5cfa5097c61cc2e5b7f502c0a95c1
gnklv <wgg357@gmail.com>
2020-12-21 04:45:00 +0500

    feat: created WeatherError and WeatherLoading components

---
+++ src/components/WeatherError.vue
@@ -0,0 +1,26 @@
+<template>
+  <p class="weather-error__message">{{ errorMsg }}</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'WeatherError',
+  props: {
+    errorMsg: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.weather-error {
+  &__message {
+    margin: auto;
+    font-size: 20px;
+  }
+}
+</style>
---
+++ src/components/WeatherLoading.vue
@@ -0,0 +1,40 @@
+<template>
+  <ui-svg-icon class="weather-loading__icon" icon-id="pin" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import UiSvgIcon from '@/components/ui/svg/UiSvgIcon.vue';
+
+export default defineComponent({
+  name: 'WeatherLoading',
+  components: { UiSvgIcon },
+});
+</script>
+
+<style lang="scss" scoped>
+.weather-loading {
+  &__icon {
+    width: 50px;
+    height: 50px;
+    margin: auto;
+    fill: #fff;
+    animation: jumping 1s infinite;
+  }
+}
+
+@keyframes jumping {
+  0% {
+    transform: translateY(0);
+  }
+
+  50% {
+    transform: translateY(-50%);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
+}
+</style>
